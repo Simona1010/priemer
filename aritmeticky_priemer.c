8622e065d00e612bc78dd400fed19cd1d88f9b96
Simona1010 <61478191+Simona1010@users.noreply.github.com>
2020-04-14 19:16:38 +0200

    Add files via upload

---
+++ aritmeticky_priemer.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#define N 3
+
+float aritmeticky_priemer(float *data,int l);
+
+int main ()
+{
+ float data[3]={1,8,6};
+
+printf("%f", aritmeticky_priemer(data,N));
+
+
+}
+
+float aritmeticky_priemer(float *data,int l)
+{
+	double suma=0;	
+	float priemer1 = 0;
+	
+	printf("Dlzka pola je %d\n", l);
+	
+	int i;
+	for( i = 0; i<l; i++)
+	{
+		suma += data[i];
+		printf("Hodnota SUMA v %d cykle je: %f\n", i, suma);
+		priemer1=suma/l;
+	}
+
+	return priemer1;
+}
